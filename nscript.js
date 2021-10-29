3068a24a377fd690a43f43b31a881372a9b0dde6
sasidhanallapati <90022300+sasidhanallapati@users.noreply.github.com>
2021-10-30 00:06:53 +0530

    Update nscript.js

--- nscript.js
+++ nscript.js
@@ -1,14 +1,14 @@
 function x() {
     var msg1 = "sasidha's";
-    console.log("x:message = "+msg1);
+    console.log("x:message = "+msg1)
 }
 x();
 
 var y = function() {
     var msg2 = "code in js";
-    console.log("y:message = "+msg2);
+    console.log("y:message = "+msg2)
 }
 y();
 
 var msg3 = "in global";
-console.log("global:message = "+msg3);
+console.log("global:message = "+msg3)
