55b0ef79f949ec8277580cc898664ce53c8d0a4e
sasidhanallapati <90022300+sasidhanallapati@users.noreply.github.com>
2021-10-30 00:09:08 +0530

    Create tscript.js

---
+++ tscript.js
@@ -0,0 +1,18 @@
+var x;
+console.log(x);
+if(x==undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log("x is defined as "+x);
+}
+
+x = "sasidha";
+if(x==undefined) {
+    console.log("x is undefined");
+}
+else {
+    console.log("x is defined as "+x );
+}
+
+console.log(y);
